06085ba71c8c845a553b45156b9738ce7c0a68ee
kenoob <keno.ob@live.com>
2014-03-22 23:38:07 +0100

    New

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+CSRF_ENABLED = True
+SECRET_KEY = 'I-know what-you-like'
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'app.db')
+
+
+
